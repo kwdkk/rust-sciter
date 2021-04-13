c17437aff27d9584c9a05c9dd0e4cdf2474dd2bc
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-04-13 11:52:19 +0900

    feat: Convert `EVENT_GROUPS` to bitflags

--- src/eventhandler.rs
+++ src/eventhandler.rs
@@ -94,19 +94,16 @@ pub(crate) extern "system" fn _event_handler_proc<T: EventHandler>(tag: LPVOID, 
 
 fn process_events(me: &mut dyn EventHandler, he: HELEMENT, evtg: UINT, params: LPVOID) -> BOOL
 {
-	let evtg : EVENT_GROUPS = unsafe { ::std::mem::transmute(evtg) };
 	if he.is_null()
-		&& evtg != EVENT_GROUPS::SUBSCRIPTIONS_REQUEST
-		&& evtg != EVENT_GROUPS::HANDLE_BEHAVIOR_EVENT
-		&& evtg != EVENT_GROUPS::HANDLE_INITIALIZATION
-		&& evtg != EVENT_GROUPS::HANDLE_SOM
+		&& evtg != EVENT_GROUPS_SUBSCRIPTIONS_REQUEST
+		&& evtg != EVENT_GROUPS::HANDLE_BEHAVIOR_EVENT.bits()
+		&& evtg != EVENT_GROUPS::HANDLE_INITIALIZATION.bits()
+		&& evtg != EVENT_GROUPS::HANDLE_SOM.bits()
 	{
-		eprintln!("[sciter] warning! null element for {:04X}", evtg as u32);
+		eprintln!("[sciter] warning! null element for {:04X}", evtg);
 	}
 
-	let result = match evtg {
-
-		EVENT_GROUPS::SUBSCRIPTIONS_REQUEST => {
+	let result = if evtg == EVENT_GROUPS_SUBSCRIPTIONS_REQUEST {
 			assert!(!params.is_null());
 			let scnm = params as *mut EVENT_GROUPS;
 			let nm = unsafe {&mut *scnm};
@@ -115,192 +112,195 @@ fn process_events(me: &mut dyn EventHandler, he: HELEMENT, evtg: UINT, params: L
 				*nm = needed;
 			}
 			handled.is_some()
-		},
+	} else {
+		let evtg = EVENT_GROUPS::from_bits_truncate(evtg);
 
-		EVENT_GROUPS::HANDLE_INITIALIZATION => {
-			assert!(!params.is_null());
-			let scnm = params as *mut INITIALIZATION_PARAMS;
-			let nm = unsafe { &mut *scnm };
-			match nm.cmd {
-				INITIALIZATION_EVENTS::BEHAVIOR_DETACH => {
-					me.detached(he);
-				},
-
-				INITIALIZATION_EVENTS::BEHAVIOR_ATTACH => {
-					me.attached(he);
-				},
-			};
-			true
-		},
-
-		EVENT_GROUPS::HANDLE_SOM => {
-			assert!(!params.is_null());
-			let scnm = params as *mut SOM_PARAMS;
-			let nm = unsafe { &mut *scnm };
-			match nm.cmd {
-				SOM_EVENTS::SOM_GET_PASSPORT => {
-					if let Some(asset) = me.get_asset() {
-						nm.result.passport = asset.get_passport();
-						return true as BOOL;
-					}
-				},
-
-				SOM_EVENTS::SOM_GET_ASSET => {
-					if let Some(asset) = me.get_asset() {
-						nm.result.asset = asset;
-						return true as BOOL;
-					}
-				},
-			};
-			false
-		},
-
-
-		EVENT_GROUPS::HANDLE_BEHAVIOR_EVENT => {
-			assert!(!params.is_null());
-			let scnm = params as *const BEHAVIOR_EVENT_PARAMS;
-			let nm = unsafe { &*scnm };
-
-      use dom::event::EventReason;
-			let code :BEHAVIOR_EVENTS = unsafe{ ::std::mem::transmute(nm.cmd & 0x0_0FFF) };
-			let phase: PHASE_MASK = unsafe { ::std::mem::transmute(nm.cmd & 0xFFFF_F000) };
-			let reason = match code {
-				BEHAVIOR_EVENTS::EDIT_VALUE_CHANGED | BEHAVIOR_EVENTS::EDIT_VALUE_CHANGING => {
-					let reason: EDIT_CHANGED_REASON = unsafe{ ::std::mem::transmute(nm.reason as UINT) };
-					EventReason::EditChanged(reason)
-				},
-
-				BEHAVIOR_EVENTS::VIDEO_BIND_RQ => {
-					EventReason::VideoBind(nm.reason as LPVOID)
-				}
-
-				_ => {
-					let reason: CLICK_REASON = unsafe{ ::std::mem::transmute(nm.reason as UINT) };
-					EventReason::General(reason)
-				}
-			};
-
-			if he.is_null() && code != BEHAVIOR_EVENTS::MEDIA_CHANGED {
-				eprintln!("[sciter] warning! null element for {:?}:{:?}", evtg, code);
-			}
-
-			if phase == PHASE_MASK::SINKING {	// catch this only once
-				match code {
-					BEHAVIOR_EVENTS::DOCUMENT_COMPLETE => {
-						me.document_complete(he, nm.heTarget);
+		match evtg {
+			EVENT_GROUPS::HANDLE_INITIALIZATION => {
+				assert!(!params.is_null());
+				let scnm = params as *mut INITIALIZATION_PARAMS;
+				let nm = unsafe { &mut *scnm };
+				match nm.cmd {
+					INITIALIZATION_EVENTS::BEHAVIOR_DETACH => {
+						me.detached(he);
 					},
-					BEHAVIOR_EVENTS::DOCUMENT_CLOSE => {
-						me.document_close(he, nm.heTarget);
+
+					INITIALIZATION_EVENTS::BEHAVIOR_ATTACH => {
+						me.attached(he);
 					},
-					_ => ()
 				};
-			}
-
-			let handled = me.on_event(he, nm.he, nm.heTarget, code, phase, reason);
-			handled
-		},
-
-		EVENT_GROUPS::HANDLE_SCRIPTING_METHOD_CALL => {
-			assert!(!params.is_null());
-			let scnm = params as *mut SCRIPTING_METHOD_PARAMS;
-			let nm = unsafe { &mut *scnm };
-			let name = u2s!(nm.name);
-			let argv = unsafe { Value::unpack_from(nm.argv, nm.argc) };
-			let rv = me.on_script_call(he, &name, &argv);
-			let handled = if let Some(v) = rv {
-				v.pack_to(&mut nm.result);
 				true
-			} else {
+			},
+
+			EVENT_GROUPS::HANDLE_SOM => {
+				assert!(!params.is_null());
+				let scnm = params as *mut SOM_PARAMS;
+				let nm = unsafe { &mut *scnm };
+				match nm.cmd {
+					SOM_EVENTS::SOM_GET_PASSPORT => {
+						if let Some(asset) = me.get_asset() {
+							nm.result.passport = asset.get_passport();
+							return true as BOOL;
+						}
+					},
+
+					SOM_EVENTS::SOM_GET_ASSET => {
+						if let Some(asset) = me.get_asset() {
+							nm.result.asset = asset;
+							return true as BOOL;
+						}
+					},
+				};
 				false
-			};
-			handled
-		},
+			},
 
-    EVENT_GROUPS::HANDLE_METHOD_CALL => {
-      assert!(!params.is_null());
-      let scnm = params as *const METHOD_PARAMS;
-      let nm = unsafe { & *scnm };
-      let code: BEHAVIOR_METHOD_IDENTIFIERS = unsafe { ::std::mem::transmute((*nm).method) };
-      use capi::scbehavior::BEHAVIOR_METHOD_IDENTIFIERS::*;
 
-      // output values
-      let mut method_value = Value::new();
-      let mut is_empty = false;
+			EVENT_GROUPS::HANDLE_BEHAVIOR_EVENT => {
+				assert!(!params.is_null());
+				let scnm = params as *const BEHAVIOR_EVENT_PARAMS;
+				let nm = unsafe { &*scnm };
 
-      let handled = {
+				use dom::event::EventReason;
+				let code :BEHAVIOR_EVENTS = unsafe{ ::std::mem::transmute(nm.cmd & 0x0_0FFF) };
+				let phase: PHASE_MASK = unsafe { ::std::mem::transmute(nm.cmd & 0xFFFF_F000) };
+				let reason = match code {
+					BEHAVIOR_EVENTS::EDIT_VALUE_CHANGED | BEHAVIOR_EVENTS::EDIT_VALUE_CHANGING => {
+						let reason: EDIT_CHANGED_REASON = unsafe{ ::std::mem::transmute(nm.reason as UINT) };
+						EventReason::EditChanged(reason)
+					},
 
-        // unpack method parameters
-        use dom::event::MethodParams;
-        let reason = match code {
-          DO_CLICK => {
-            MethodParams::Click
-          },
-          IS_EMPTY => {
-            MethodParams::IsEmpty(&mut is_empty)
-          },
-          GET_VALUE => {
-            MethodParams::GetValue(&mut method_value)
-          },
-          SET_VALUE => {
-            // Value from Sciter.
-            let payload = params as *const VALUE_PARAMS;
-            let pm = unsafe { & *payload };
-            MethodParams::SetValue(Value::from(&pm.value))
-          },
+					BEHAVIOR_EVENTS::VIDEO_BIND_RQ => {
+						EventReason::VideoBind(nm.reason as LPVOID)
+					}
 
-          _ => {
-            MethodParams::Custom((*nm).method, params)
-          },
-        };
+					_ => {
+						let reason: CLICK_REASON = unsafe{ ::std::mem::transmute(nm.reason as UINT) };
+						EventReason::General(reason)
+					}
+				};
 
-        // call event handler
-        let handled = me.on_method_call(he, reason);
-        handled
-      };
+				if he.is_null() && code != BEHAVIOR_EVENTS::MEDIA_CHANGED {
+					eprintln!("[sciter] warning! null element for {:?}:{:?}", evtg, code);
+				}
 
-      if handled {
-        // Pack values back to Sciter.
-        match code {
-          GET_VALUE => {
-            let payload = params as *mut VALUE_PARAMS;
-            let pm = unsafe { &mut *payload };
-            method_value.pack_to(&mut pm.value);
-          },
+				if phase == PHASE_MASK::SINKING {	// catch this only once
+					match code {
+						BEHAVIOR_EVENTS::DOCUMENT_COMPLETE => {
+							me.document_complete(he, nm.heTarget);
+						},
+						BEHAVIOR_EVENTS::DOCUMENT_CLOSE => {
+							me.document_close(he, nm.heTarget);
+						},
+						_ => ()
+					};
+				}
 
-          IS_EMPTY => {
-            let payload = params as *mut IS_EMPTY_PARAMS;
-            let pm = unsafe { &mut *payload };
-            pm.is_empty = is_empty as UINT;
-          },
+				let handled = me.on_event(he, nm.he, nm.heTarget, code, phase, reason);
+				handled
+			},
 
-          _ => {},
-        }
-      }
-      // we've done here
-      handled
-    },
+			EVENT_GROUPS::HANDLE_SCRIPTING_METHOD_CALL => {
+				assert!(!params.is_null());
+				let scnm = params as *mut SCRIPTING_METHOD_PARAMS;
+				let nm = unsafe { &mut *scnm };
+				let name = u2s!(nm.name);
+				let argv = unsafe { Value::unpack_from(nm.argv, nm.argc) };
+				let rv = me.on_script_call(he, &name, &argv);
+				let handled = if let Some(v) = rv {
+					v.pack_to(&mut nm.result);
+					true
+				} else {
+					false
+				};
+				handled
+			},
 
-		EVENT_GROUPS::HANDLE_TIMER => {
-			assert!(!params.is_null());
-			let scnm = params as *const TIMER_PARAMS;
-			let nm = unsafe { & *scnm };
-			let handled = me.on_timer(he, nm.timerId as u64);
-			handled
-		},
+			EVENT_GROUPS::HANDLE_METHOD_CALL => {
+				assert!(!params.is_null());
+				let scnm = params as *const METHOD_PARAMS;
+				let nm = unsafe { & *scnm };
+				let code: BEHAVIOR_METHOD_IDENTIFIERS = unsafe { ::std::mem::transmute((*nm).method) };
+				use capi::scbehavior::BEHAVIOR_METHOD_IDENTIFIERS::*;
 
-		EVENT_GROUPS::HANDLE_DRAW => {
-			assert!(!params.is_null());
-			let scnm = params as *const DRAW_PARAMS;
-			let nm = unsafe { & *scnm };
-			let handled = me.on_draw(he, nm.gfx, &nm.area, nm.layer);
-			handled
-		},
+				// output values
+				let mut method_value = Value::new();
+				let mut is_empty = false;
 
-		// unknown `EVENT_GROUPS` notification
-		_ => {
-			eprintln!("[sciter] warning! unknown event group {:04X}", evtg as u32);
-			false
-		},
+				let handled = {
+
+					// unpack method parameters
+					use dom::event::MethodParams;
+					let reason = match code {
+						DO_CLICK => {
+							MethodParams::Click
+						},
+						IS_EMPTY => {
+							MethodParams::IsEmpty(&mut is_empty)
+						},
+						GET_VALUE => {
+							MethodParams::GetValue(&mut method_value)
+						},
+						SET_VALUE => {
+							// Value from Sciter.
+							let payload = params as *const VALUE_PARAMS;
+							let pm = unsafe { & *payload };
+							MethodParams::SetValue(Value::from(&pm.value))
+						},
+
+						_ => {
+							MethodParams::Custom((*nm).method, params)
+						},
+					};
+
+					// call event handler
+					let handled = me.on_method_call(he, reason);
+					handled
+				};
+
+				if handled {
+					// Pack values back to Sciter.
+					match code {
+						GET_VALUE => {
+							let payload = params as *mut VALUE_PARAMS;
+							let pm = unsafe { &mut *payload };
+							method_value.pack_to(&mut pm.value);
+						},
+
+						IS_EMPTY => {
+							let payload = params as *mut IS_EMPTY_PARAMS;
+							let pm = unsafe { &mut *payload };
+							pm.is_empty = is_empty as UINT;
+						},
+
+						_ => {},
+					}
+				}
+				// we've done here
+				handled
+			},
+
+			EVENT_GROUPS::HANDLE_TIMER => {
+				assert!(!params.is_null());
+				let scnm = params as *const TIMER_PARAMS;
+				let nm = unsafe { & *scnm };
+				let handled = me.on_timer(he, nm.timerId as u64);
+				handled
+			},
+
+			EVENT_GROUPS::HANDLE_DRAW => {
+				assert!(!params.is_null());
+				let scnm = params as *const DRAW_PARAMS;
+				let nm = unsafe { & *scnm };
+				let handled = me.on_draw(he, nm.gfx, &nm.area, nm.layer);
+				handled
+			},
+
+			// unknown `EVENT_GROUPS` notification
+			_ => {
+				eprintln!("[sciter] warning! unknown event group {:04X}", evtg.bits());
+				false
+			},
+		}
 	};
 	return result as BOOL;
 }
